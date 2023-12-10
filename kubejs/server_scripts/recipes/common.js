82ebc7d6ff8622ff6e5dfd68f61f8ce2d16f5093
Neha YoruNina <836193631@qq.com>
2023-12-10 21:03:22 +0800

    添加Boss相关内容兼容

--- kubejs/server_scripts/recipes/common.js
+++ kubejs/server_scripts/recipes/common.js
@@ -1,3 +1,10 @@
 ServerEvents.recipes(event => {
     event.remove({ mod: 'nameless_trinkets' })
+    event.remove({ mod: 'somebosses'})
+    event.remove({ output: 'supplementaries:candy' })
+    event.shaped('minecraft:elytra', [
+        ['', 'alexsmobs:banana_slug_slime', ''],
+        ['minecraft:phantom_membrane', 'createaddition:gold_wire', 'minecraft:phantom_membrane'],
+        ['', 'alexsmobs:banana_slug_slime', '']
+    ])
 })
