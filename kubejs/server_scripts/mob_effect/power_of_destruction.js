4163f9e26b6225ab3a13fe543ff3c6d4e0bb7fe8
201220049 <201220049@smail.nju.edu.cn>
2024-02-13 00:43:14 +0800

    修复崩毁之力的数值
    修复崩毁之力的数值

--- kubejs/server_scripts/mob_effect/power_of_destruction.js
+++ kubejs/server_scripts/mob_effect/power_of_destruction.js
@@ -16,21 +16,21 @@ function powerOfDestructionPlayerHurtByOthers(event, data) {
              let amplifier = entity.getEffect('kubejs:power_of_destruction').getAmplifier()
              player.potionEffects.add('goety:busted', 20 * 8, amplifier)
              entity.potionEffects.add('minecraft:strength', 20 * 8, (amplifier + 1))
-             event.amount = event.amount + Math.floor(player.getMaxHealth()*0.5*amplifier)
+             event.amount = event.amount + Math.floor(player.getMaxHealth()*0.5*(amplifier+1))
          }
          else if(0 < player.getArmorValue() < 20)
          {
             let amplifier = entity.getEffect('kubejs:power_of_destruction').getAmplifier()
             player.potionEffects.add('goety:busted', 20 * 8, (amplifier + 1))
             entity.potionEffects.add('minecraft:strength', 20 * 8, (amplifier + 3))
-            event.amount = event.amount + Math.floor(player.getMaxHealth()*1.5*amplifier)
+            event.amount = event.amount + Math.floor(player.getMaxHealth()*1.5*(amplifier+1))
          }
          else if(player.getArmorValue() <= 0)
          {
             let amplifier = entity.getEffect('kubejs:power_of_destruction').getAmplifier()
             player.potionEffects.add('goety:busted', 20 * 8, (amplifier + 2))
             entity.potionEffects.add('minecraft:strength', 20 * 8, (amplifier + 5))
-            event.amount = event.amount + Math.floor(player.getMaxHealth()*2.5*amplifier)
+            event.amount = event.amount + Math.floor(player.getMaxHealth()*2.5*(amplifier+1))
          }
     }
     
