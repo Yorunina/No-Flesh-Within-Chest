a4c733e75ed90dff93e6e5750ee32f6f2e3878c9
Neha YoruNina <836193631@qq.com>
2024-02-22 02:50:48 +0800

    增加部分文本汉化

--- kubejs/server_scripts/recipes/cutting.js
+++ kubejs/server_scripts/recipes/cutting.js
@@ -19,5 +19,7 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'minecraft:melon_slice', mod: 'farmersdelight' })
 
     registerCustomRecipe(new CuttingRecipe([Item.of('minecraft:melon')], [{ "count": 7, "item": "minecraft:melon_slice" }, { "chance": 0.05, "item": "kubejs:watermelon_organ" }]))
+
+    registerCustomRecipe(new CuttingRecipe([Item.of('extradelight:sliced_ginger')], [{ "count": 1, "item": "extradelight:grated_ginger" }, { "chance": 0.2, "item": "extradelight:grated_ginger" }]))
 })
 
