0bb755aba2ebdb7abd84cc45cc4131f0065d4b17
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2024-02-23 23:24:54 +0800

    Update isb_compat.js

--- kubejs/server_scripts/utils/isb_compat.js
+++ kubejs/server_scripts/utils/isb_compat.js
@@ -5,6 +5,11 @@ function getPlayerMagicData(player) {
     return $MagicData.getPlayerMagicData(player)
 }
 
-function randomGet(list) {
-    return list[Math.floor(Math.random() * list.length)];
+function getShuffledList(list) {
+    //shuffle
+    for(let i = list.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list;
 }
