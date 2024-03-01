39f4c25eb755352ba11cfeaa7e3accad290c32f5
Neha YoruNina <836193631@qq.com>
2024-03-02 06:21:28 +0800

    修复wares的异常问题

--- kubejs/startup_scripts/key_bind_register.js
+++ kubejs/startup_scripts/key_bind_register.js
@@ -4,6 +4,4 @@ const $KeyMappingRegistry = Java.loadClass('dev.architectury.registry.client.key
 
 global.OrganSkill = new $KeyMapping(`key.kubejs.organ_skill`, $GLFWKey.GLFW_KEY_X, 'key.categories.kubejs')
 
-ClientEvents.init(event => {
-    $KeyMappingRegistry.register(global.OrganSkill)
-})
+$KeyMappingRegistry.register(global.OrganSkill)
