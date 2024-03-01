336ceab767fe21698cc69a5cd03ea38eb2d01ab6
Neha YoruNina <836193631@qq.com>
2024-03-02 04:06:25 +0800

    龙化效果平衡性修正

--- kubejs/server_scripts/mob_effect/dragon_power.js
+++ kubejs/server_scripts/mob_effect/dragon_power.js
@@ -7,15 +7,17 @@
  */
 function dragonPowerPlayerHurtByOthers(event, data) {
     let player = event.entity;
+    if (!event.source.actual) return
     if (!player.hasEffect('kubejs:dragon_power')) {
         return
     }
     let dragonPowerEffect = player.getEffect('kubejs:dragon_power')
     let amplify = dragonPowerEffect.getAmplifier()
+    let maxExtraHealth = player.getMaxHealth() * 3
     if (amplify < 5) {
         event.amount = event.amount * (0.8 - amplify * 0.2)
-    } else if (player.absorptionAmount < player.maxHealth * 2) {
-        player.absorptionAmount = player.absorptionAmount + event.amount * (amplify - 4) * 0.2
+    } else if (player.absorptionAmount < maxExtraHealth) {
+        player.absorptionAmount = Math.min(player.absorptionAmount + event.amount * (amplify - 4) * 0.2, maxExtraHealth)
         event.amount = 0
     }
 }
