3c967df4230ec539ce8b44723edd2bf060ce69f1
李诗琪 <120086484+LunaGlaze@users.noreply.github.com>
2024-06-30 23:10:16 +0800

    添加优先级并重构超限施法逻辑
    通过仿照器官激活与特殊效果，编写了新版器官超限施法逻辑；该项改动能使社区创作内容在不修改本体内容（造成冲突）的情况下添加可以影响器官超限施法等级的器官，通过为器官添加'kubejs:overmagic_only'或'kubejs:overmagic'标签，能够执行相应函数，根据输出结果对超限施法等级进行调整

--- kubejs/server_scripts/utils/isb_compat.js
+++ kubejs/server_scripts/utils/isb_compat.js
@@ -1,3 +1,4 @@
+// priority: 10
 const $AttributeRegistry = Java.loadClass("io.redspace.ironsspellbooks.api.registry.AttributeRegistry")
 const $MagicData = Java.loadClass("io.redspace.ironsspellbooks.api.magic.MagicData")
 const $SpellRegistry = Java.loadClass("io.redspace.ironsspellbooks.api.registry.SpellRegistry")
