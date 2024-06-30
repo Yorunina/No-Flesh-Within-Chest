451f54262b6accbf59d361773463a65ce26cf1a9
李诗琪 <120086484+LunaGlaze@users.noreply.github.com>
2024-06-30 23:38:40 +0800

    修复少量bug并为社区内容添加新的支持 (#221)
    * 为global.js添加优先级
    
    该项目可以保证若有社区内容需要添加新的激活属性类型时，linux系统可以正确先加载本体文件，并解决由此引发的启动崩溃
    
    * 修复新月细剑属性bug
    
    创建'irons_spells_js:magic_sword'物品时，为其添加属性的部件为addAdditionalAttribute而非modifyAttribute
    
    * 为器官添加新tag以便新系统工作
    
    详情见isb_compat.js与lucky_block.js
    
    * 修改幸运方块器官掉落逻辑
    
    将原先的禁止单物品替换为当存在某一tag时禁止掉落；此项改动能使社区创作内容可以在不修改本体文件（导致冲突）的情况下通过为器官添加'kubejs:disluckloot'标签禁止特定器官通过幸运方块掉落
    
    * 修改超限施法逻辑
    
    通过仿照器官激活与特殊效果，编写了新版器官超限施法逻辑；该项改动能使社区创作内容在不修改本体内容（造成冲突）的情况下添加可以影响器官超限施法等级的器官，通过为器官添加'kubejs:overmagic_only'或'kubejs:overmagic'标签，能够执行相应函数，根据输出结果对超限施法等级进行调整
    
    * 添加优先级并重构超限施法逻辑
    
    通过仿照器官激活与特殊效果，编写了新版器官超限施法逻辑；该项改动能使社区创作内容在不修改本体内容（造成冲突）的情况下添加可以影响器官超限施法等级的器官，通过为器官添加'kubejs:overmagic_only'或'kubejs:overmagic'标签，能够执行相应函数，根据输出结果对超限施法等级进行调整
    
    * 为激活效果提供最大扭曲值支持
    
    该项修改能使得社区内容若添加了某种器官使得激活状态下最大扭曲值有所变化的情况下，不会导致最大扭曲值无限增大或减小带来的位置问题

--- kubejs/server_scripts/utils/isb_compat.js
+++ kubejs/server_scripts/utils/isb_compat.js
@@ -1,3 +1,4 @@
+// priority: 10
 const $AttributeRegistry = Java.loadClass("io.redspace.ironsspellbooks.api.registry.AttributeRegistry")
 const $MagicData = Java.loadClass("io.redspace.ironsspellbooks.api.magic.MagicData")
 const $SpellRegistry = Java.loadClass("io.redspace.ironsspellbooks.api.registry.SpellRegistry")
@@ -13,10 +14,44 @@ function randomGet(list) {
 }
 
 function overLimitSpellCast(resourceLocation, amplifier, player, consume) {
-    let itemMap = getPlayerChestCavityItemMap(player)
     let typeMap = getPlayerChestCavityTypeMap(player)
-    if (itemMap.has('kubejs:plastic_heart') && typeMap.has('kubejs:magic')) {
-        amplifier = amplifier + typeMap.get('kubejs:magic').length
+    let onlySet = new Set()
+    if (typeMap.has('kubejs:overmagic_only')) {
+        typeMap.get('kubejs:overmagic_only').forEach(organ => {
+            if (!onlySet.has(organ.id)) {
+                onlySet.add(organ.id)
+                amplifier = amplifier + organOverLimitMagicOnlyStrategies[organ.id](player, organ)
+            }
+        })
+    }
+    if (typeMap.has('kubejs:overmagic')) {
+        typeMap.get('kubejs:overmagic').forEach(organ => {
+            amplifier = amplifier + organOverLimitMagicStrategies[organ.id](player, organ)
+        })
     }
     $SpellRegistry["getSpell(net.minecraft.resources.ResourceLocation)"](resourceLocation).attemptInitiateCast(Item.of('air'), amplifier, player.level, player, $CastSource.NONE, consume, "main_hand")
 }
+
+/**
+ * 超限施法等级策略
+ * @constant
+ * @type {Object<string,function(Internal.ServerPlayer, organ):Float>}
+ */
+const organOverLimitMagicStrategies = {
+
+};
+/**
+ * 唯一超限施法等级策略
+ * @constant
+ * @type {Object<string,function(Internal.ServerPlayer, organ):Float>}
+ */
+const organOverLimitMagicOnlyStrategies = {
+    'kubejs:plastic_heart': function (player, organ) {
+        let typeMap = getPlayerChestCavityTypeMap(player)
+        let amplifier = 0
+        if (typeMap.has('kubejs:magic')) {
+            amplifier = typeMap.get('kubejs:magic').length
+        }
+        return amplifier;
+    },
+};
